f9ad8d76ca9a1b568bd6a6cd68de49639a9b21be
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2026-05-01 15:16:47 +0800

    feat: add daily forecast card.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getWeather } from "./api/openWeatherApi";
+import Card from "./components/Card";
+import DailyForecast from "./components/DailyForecast";
 
 function App() {
   const { data } = useQuery({
@@ -10,7 +12,15 @@ function App() {
         longitude: -74.006,
       }),
   });
-  return <>{JSON.stringify(data)}</>;
+  return (
+    <div className="flex flex-col gap-8 p-4 text-white">
+      <Card title="Current Weather">{JSON.stringify(data?.current)}</Card>
+      <Card title="Hourly Forecast (48 Hours)">
+        {JSON.stringify(data?.hourly)}
+      </Card>
+      <DailyForecast />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/DailyForecast.tsx
@@ -0,0 +1,46 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import Card from "./Card";
+import { getWeather } from "../api/openWeatherApi";
+
+type Props = {};
+
+const DailyForecast = (props: Props) => {
+  const { data } = useSuspenseQuery({
+    queryKey: ["weather"],
+    queryFn: () =>
+      getWeather({
+        latitude: 40.7128,
+        longitude: -74.006,
+      }),
+  });
+
+  return (
+    <Card title="Daily Forecast">
+      <div className="flex flex-col gap-4">
+        {data?.daily.map((day) => (
+          <div key={day.dt} className="flex justify-between">
+            <p className="w-9">
+              {new Date(day.dt * 1000).toLocaleDateString(undefined, {
+                weekday: "short",
+              })}
+            </p>
+            <img
+              className="size-9"
+              src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+              alt="Weather Icon"
+            />
+            <p className="w-10 text-center ">{Math.round(day.temp.day)}°C</p>
+            <p className="text-gray-500/75 w-10 text-center">
+              {Math.round(day.temp.min)}°C
+            </p>
+            <p className="text-gray-500/75 w-10 text-center">
+              {Math.round(day.temp.max)}°C
+            </p>
+          </div>
+        ))}
+      </div>
+    </Card>
+  );
+};
+
+export default DailyForecast;
