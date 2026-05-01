f265e5e9fafac4c02ddfd0b6abe3704e5c52bdf7
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2026-05-01 15:22:01 +0800

    chore: remove redundant elements.

--- src/components/DailyForecast.tsx
+++ src/components/DailyForecast.tsx
@@ -15,30 +15,30 @@ const DailyForecast = (props: Props) => {
   });
 
   return (
-    <Card title="Daily Forecast">
-      <div className="flex flex-col gap-4">
-        {data?.daily.map((day) => (
-          <div key={day.dt} className="flex justify-between">
-            <p className="w-9">
-              {new Date(day.dt * 1000).toLocaleDateString(undefined, {
-                weekday: "short",
-              })}
-            </p>
-            <img
-              className="size-9"
-              src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
-              alt="Weather Icon"
-            />
-            <p className="w-10 text-center ">{Math.round(day.temp.day)}°C</p>
-            <p className="text-gray-500/75 w-10 text-center">
-              {Math.round(day.temp.min)}°C
-            </p>
-            <p className="text-gray-500/75 w-10 text-center">
-              {Math.round(day.temp.max)}°C
-            </p>
-          </div>
-        ))}
-      </div>
+    <Card title="Daily Forecast" childrenClassName="flex flex-col gap-4">
+      {data?.daily.map((day) => (
+        <div key={day.dt} className="flex justify-between">
+          <p className="w-9">
+            {new Date(day.dt * 1000).toLocaleDateString(undefined, {
+              weekday: "short",
+            })}
+          </p>
+          <img
+            className="size-9"
+            src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+            alt="Weather Icon"
+          />
+          <p className="w-10 tabular-nums text-right ">
+            {Math.round(day.temp.day)}°C
+          </p>
+          <p className="text-gray-500/75 w-10 tabular-nums text-right">
+            {Math.round(day.temp.min)}°C
+          </p>
+          <p className="text-gray-500/75 w-10 tabular-nums text-right">
+            {Math.round(day.temp.max)}°C
+          </p>
+        </div>
+      ))}
     </Card>
   );
 };
